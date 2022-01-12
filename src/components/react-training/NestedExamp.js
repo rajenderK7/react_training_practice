54fe90dd4e713f3c4bdc454c3a4114ed943d9ac2
Rajender Katkuri <rajenderkatkuri123@gmail.com>
2022-01-12 20:35:17 +0530

    assignment-2 stable

--- src/components/react-training/NestedExamp.js
+++ src/components/react-training/NestedExamp.js
@@ -1,5 +1,6 @@
 const NestedExamp = () => {
   return (
+    //   text to go inside container
     <div>
       <h1 className="p-2 text-white">
         This is a Nested Component which is again nested in another Component
