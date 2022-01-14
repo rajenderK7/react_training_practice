113bcd4eed9f115c8a3c5a6cc46c322f53bdb3de
Rajender Katkuri <rajenderkatkuri123@gmail.com>
2022-01-14 23:30:38 +0530

    stable 14/01/22

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
-import Assignment2a from "./components/react-training/Assignment2a";
+import MainScreen from "./components/MainScreen";
 
 function App() {
   return (
     <div className="App">
-      {/* <MainScreen></MainScreen> */}
+      <MainScreen></MainScreen>
       {/* <Assignment1a></Assignment1a> */}
-      <p className="container w-75 para my-3" style={{ fontSize: "1.5rem" }}>
+      {/* <p className="container w-75 para my-3" style={{ fontSize: "1.5rem" }}>
         The below Form takes 3 inputs and evertime the form is submitted
         (created) the state is upadated and it is logged onto the console as an
         object. Please open console.
       </p>
-      <Assignment2a></Assignment2a>
+      <Assignment2a></Assignment2a> */}
     </div>
   );
 }
--- src/components/goal_form/GoalForm.js
+++ src/components/goal_form/GoalForm.js
@@ -3,14 +3,18 @@ import "./GoalForm.css";
 
 const GoalForm = (props) => {
   const [enteredGoal, setEnteredGoal] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const changeGoalHandler = (event) => {
+    setIsValid(true);
     setEnteredGoal(event.target.value);
   };
 
   const formSubitHandler = (event) => {
     event.preventDefault();
-    if (enteredGoal.length === 0) {
+    if (enteredGoal.trim().length === 0) {
+      setIsValid(false);
+      setEnteredGoal("");
       return;
     }
     const newGoal = {
@@ -25,9 +29,13 @@ const GoalForm = (props) => {
     <div className="goal-form container my-4 w-75 rounded border border-secondary shadow">
       <form onSubmit={formSubitHandler}>
         <div className="p-2">
-          <label className="row py-1">Goal</label>
+          <label className={`row py-1 ${isValid ? "" : "text-danger"}`}>
+            Goal
+          </label>
           <input
-            className="row form-control border border-dark"
+            className={`row form-control border border-dark ${
+              isValid ? "" : "bg-danger"
+            }`}
             type="text"
             onChange={changeGoalHandler}
             value={enteredGoal}
